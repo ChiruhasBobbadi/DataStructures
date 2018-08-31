d99bd36ec70820b8ec6883329eb9f369b9a16da5
Chiruhas Bobbadi <40091219+ChiruhasBobbadi@users.noreply.github.com>
2018-08-31 11:39:09 +0530

    bug fixes

--- PercentageCheck.java
+++ PercentageCheck.java
@@ -1,8 +1,8 @@
 import java.util.*;
-public class HelloWorld{
-   static String result[]; 
+public class Main{
+   
       static int n;
-    
+  static     String result[] = new String[10];
     static int j=0;
      public static void main(String []args){
          int n1;
@@ -25,13 +25,14 @@ public class HelloWorld{
      }
      
      static void  calculate(String names[] , String atd[])
-     {   int p,a,m,t;
+     {   float p,a,m,t;
      p=0;
      a=0;
      m=0;
      t=0;
          for(int i=0;i<n;i++)
          {
+			
              String s = atd[i];
              t=s.length();
              for(int j=0;j<s.length();j++)
@@ -39,28 +40,29 @@ public class HelloWorld{
              
              if(c=='A')
              a++;
-             else
-             if(c=='P')
+            if(c=='P')
              p++;
-             else
-             if(c=='M')
+            if(c=='M')
              m++;
              }
              t = t-m;
-             
-             double per = ((p)/t)*100;
+             ystem.out.println(p+" "+a+" "+t);
+            float per = (float)(p/t)*100;
              if(!(per>75))
              {
-                 result[j]=s;
+                 result[j]=names[i];
                  j++;
              }
              
              a=0;
              p=0;
              m=0;
+		 }
              
+      for(int i=0;result[i]!=null;i++)
+      System.out.println(result[i]);
              
              
          }
      }
-}
+
